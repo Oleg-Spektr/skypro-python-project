07dc1b1bbc50f99e1cb2f73ff6b8b217a23bfb02
имя фамилия < >
2026-05-09 09:45:54 +0300

    test: add verification script for processing functions

--- homework/tests/test.py
+++ homework/tests/test.py
@@ -1,22 +1,39 @@
-from homework.src.widget import mask_account_card
+from homework.src.processing import filter_by_state, sort_by_date
 
-# Список всех тестовых значений
-test_data = [
-    "Maestro 1596837868705199",
-    "Счет 64686473678894779589",
-    "MasterCard 7158300734726758",
-    "Счет 35383033474447895560",
-    "Visa Classic 6831982476737658",
-    "Visa Platinum 8990922113665229",
-    "Visa Gold 5999414228426353",
-    "Счет 73654108430135874305"
+# Тестовые данные
+data = [
+    {'id': 414288290, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
 
-print("--- Результаты маскировки ---")
-for item in test_data:
-    result = mask_account_card(item)
-    print(f"{item} -> {result}")
+def test_processing():
+    # 1. Тест фильтрации по умолчанию (EXECUTED)
+    filtered_executed = filter_by_state(data)
+    assert len(filtered_executed) == 2
+    assert filtered_executed[0]['state'] == 'EXECUTED'
+    print("Тест filter_by_state (default) пройден!")
 
-from homework.src.widget import get_date
+    # 2. Тест фильтрации по статусу CANCELED
+    filtered_canceled = filter_by_state(data, 'CANCELED')
+    assert len(filtered_canceled) == 2
+    assert filtered_canceled[0]['state'] == 'CANCELED'
+    print("Тест filter_by_state (CANCELED) пройден!")
 
-print(get_date("2024-03-11T02:26:18.671407"))  # Вывод: 11.03.2024
+    # 3. Тест сортировки по дате (по убыванию - по умолчанию)
+    sorted_data = sort_by_date(data)
+    assert sorted_data[0]['id'] == 414288290  # Самая свежая дата (2019 год)
+    print("Тест sort_by_date (descending) пройден!")
+
+    # 4. Тест сортировки по дате (по возрастанию)
+    sorted_data_asc = sort_by_date(data, descending=False)
+    assert sorted_data_asc[0]['id'] == 939719570  # Самая старая дата (июнь 2018)
+    print("Тест sort_by_date (ascending) пройден!")
+
+if __name__ == "__main__":
+    try:
+        test_processing()
+        print("\nВсе тесты успешно завершены!")
+    except AssertionError as e:
+        print(f"\nОшибка в тестах!")
